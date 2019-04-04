6b5a789b7c7e59db5792fe606e34eb127f3f1925
kumaresan <kumaresan.manickavelu@gmail.com>
2019-04-04 16:21:03 -0700

    all menus

--- src/routes.js
+++ src/routes.js
@@ -2,16 +2,20 @@
 import Dashboard from "@material-ui/icons/Dashboard";
 import Person from "@material-ui/icons/Person";
 import Alert from "@material-ui/icons/Language";
-//import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
 //import BubbleChart from "@material-ui/icons/BubbleChart";
 //import LocationOn from "@material-ui/icons/LocationOn";
-//import Notifications from "@material-ui/icons/Notifications";
-//import Unarchive from "@material-ui/icons/Unarchive";
+import Notifications from "@material-ui/icons/Notifications";
+import Unarchive from "@material-ui/icons/Unarchive";
 //import Language from "@material-ui/icons/Language";
 // core components/views for Admin layout
 import DashboardPage from "views/Dashboard/Dashboard.jsx";
 import NewViolations from "views/NewViolations/NewViolations.jsx";
-import UserProfile from "views/UserProfile/UserProfile.jsx";
+import KnownViolations from "views/KnownViolations/KnownViolations.jsx";
+import ConnectionCheck from "views/ConnectionCheck/ConnectionCheck.jsx";
+import RestorePolicies from "views/RestorePolicies/RestorePolicies.jsx";
+import CriticalApplications from "views/CriticalApplications/CriticalApplications.jsx";
+import Users from "views/Users/Users.jsx";
 
 //import TableList from "views/TableList/TableList.jsx";
 //import Typography from "views/Typography/Typography.jsx";
@@ -34,17 +38,44 @@ const dashboardRoutes = [
   {
     path: "/newviolations",
     name: "New Violations",
-    rtlName: "ملف تعريفي للمستخدم",
-    icon: Alert,
+    icon: Notifications,
     component: NewViolations,
     layout: "/admin"
   },
   {
+    path: "/knownviolations",
+    name: "Known Violations",
+    icon: Notifications,
+    component: KnownViolations,
+    layout: "/admin"
+  },
+  {
+    path: "/connectioncheck",
+    name: "Connection Check",
+    icon: Alert,
+    component: ConnectionCheck,
+    layout: "/admin"
+  },
+  {
+    path: "/restorepolicies",
+    name: "Restore Policies",
+    icon: Unarchive,
+    component: RestorePolicies,
+    layout: "/admin"
+  },
+  {
+    path: "/criticalapplications",
+    name: "Critical Applications",
+    icon: LibraryBooks,
+    component: CriticalApplications,
+    layout: "/admin"
+  },
+  {
     path: "/user",
-    name: "User Profile",
+    name: "Users",
     rtlName: "ملف تعريفي للمستخدم",
     icon: Person,
-    component: UserProfile,
+    component: Users,
     layout: "/admin"
   },
 /*  {
